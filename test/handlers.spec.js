a17d13ad77b670eab0c450ec2980683d318cf23e
Pavel Titov <spirokky@yandex.ru>
2018-12-10 14:58:51 +0300

    add index post handler test

--- test/handlers.spec.js
+++ test/handlers.spec.js
@@ -1,13 +1,38 @@
 const request = require("supertest");
 const app = require("../app");
-const { indexGetHandler } = require("../routes/handlers");
+const { indexGetHandler, indexPostHandler } = require("../routes/handlers");
 
 app.get("/", indexGetHandler);
+app.post("/", indexPostHandler);
 
 describe("GET /", function() {
   it("should respond with status 200", function(done) {
     request(app)
       .get("/")
       .expect(200, done);
+  });
+});
+
+describe("POST /", function() {
+  it("should respond with status 200 if url valid", function(done) {
+    request(app)
+      .post("/")
+      .send({ url: "https://google.com" })
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it("should respond with status 400 if url invalid", function(done) {
+    request(app)
+      .post("/")
+      .send({ url: "https://google.com" })
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        done();
+      });
   });
 });
